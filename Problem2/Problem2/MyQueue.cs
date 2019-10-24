c93a65ba073b328403ae53ffdb84f14402b10e53
minhnghia0512 <55732490+monster25101@users.noreply.github.com>
2019-10-24 23:42:27 +0700

    yareyare

---
+++ Problem2/Problem2/MyQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    public class MyQueue
+    {
+        private int[] Buffer;
+        private int Top;
+        private int last;
+        public MyQueue(int n)
+        {
+            Buffer = new int[n];
+            Top = -1;
+            last = 0;
+        }
+        public void Enqueue(int item)
+        {
+            Buffer[Top+1] = item;
+            Top++;
+        }
+        public int Dequeue()
+        {
+            return Buffer[last++];
+        }
+        public int Count()
+        {
+            return Top +1 ;
+        }
+        public int Peek()
+        {
+            return Buffer[last];
+        }
+    }
+}
---
+++ Problem2/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem2;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void check1()
+        {
+            MyQueue s1 = new MyQueue(10);
+            s1.Enqueue(1);
+            s1.Enqueue(2);
+            s1.Enqueue(3);
+            //Assert.AreEqual(3, s1.Count());
+            Assert.AreEqual(1, s1.Dequeue());
+        }
+    }
+}
